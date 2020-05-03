cc883acf336e74649ea523a544dade104d120795
Arkadip <in2arkadipb13@gmail.com>
2020-05-03 20:47:44 +0530

    feat: routes completed

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import { HOME } from './route';
+
+import Home from '../pages/Home';
+
+
 const RouteComponent = () => {
     return (
         <div>
             <Router>
                 <Switch>
-                    <Route path={} exact component={} />
+                    <Route path={HOME} exact component={Home} />
                     <Route path={} exact component={} />
                     {/* <Route path="*" component={}/> 404 */}
                 </Switch>
