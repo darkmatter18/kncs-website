abbe7e7940f09dede5297e1e6ca18c3bb382798f
Arkadip <in2arkadipb13@gmail.com>
2020-05-03 20:51:37 +0530

    feat: router integrated

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { CssBaseline } from '@material-ui/core';
+import RouteComponent from './routes';
+
+const App = () => (
+    <React.Fragment>
+        <CssBaseline />
+        <RouteComponent />
+    </React.Fragment>
+);
