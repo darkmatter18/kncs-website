dc0b9aa2e295e2abb4d757b9722dcfdb56580a41
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-05-15 12:36:39 +0530

    feat: users role added
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- backend/logics_xGRFqrAg3wMfGUM5js1x/login.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/login.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Login Pipe Line
+ * Created by: Arkadip Bhattachaya 15/05/2020 12.17PM
+ */
+
 function chechRecapta($re_response){
     if(isset($re_response) && !empty($re_response)){
         $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$_SERVER['HTTP_RECAPTA_SERECT'].'&response='.$re_response.'&remoteip');
@@ -12,7 +17,7 @@ function chechRecapta($re_response){
 
 function login($pdocon, $id, $password){
     //Check if admin already exist or not
-    $smt = $pdocon->prepare("SELECT id, password FROM `users-login` WHERE id =:id LIMIT 1");
+    $smt = $pdocon->prepare("SELECT id, password FROM `users_login` WHERE id =:id LIMIT 1");
     $smt->bindParam(':id', $id, PDO::PARAM_STR);
     $smt->execute();
     if($smt->rowCount() > 0){
@@ -22,7 +27,7 @@ function login($pdocon, $id, $password){
 
         if(password_verify($password,$hashed)){
             //loggedin
-            $smt = $pdocon->prepare("UPDATE `users-login` SET `last-login-ip`= :ip WHERE id = :id");
+            $smt = $pdocon->prepare("UPDATE `users_login` SET `last-login-ip`= :ip WHERE id = :id");
             $time = time();
             $ip = get_client_ip();
             $smt->bindParam(':ip', $ip, PDO::PARAM_STR);
@@ -33,6 +38,17 @@ function login($pdocon, $id, $password){
         }
     }
     return false;
+}
+
+function get_user_role($pdocon, $id){
+    $smt = $pdocon->prepare("SELECT role FROM `users_role` WHERE id= :id");
+    $smt->bindParam(':id', $id, PDO::PARAM_STR);
+    $smt->execute();
+    if($smt->rowCount() > 0){
+        $_d  = $smt->fetch(PDO::FETCH_ASSOC);
+        return $_d['role'];
+    }
+    return;
 }
 
 
@@ -57,33 +73,36 @@ if (isset($_POST['id']) && isset($_POST['password']) && isset($_POST['g_recaptch
         $password_clean = Filter::String($_POST['password']);
 
         if(login($pdocon, $id_clean, $password_clean)){
-
-            //JWT config
-            $issusedAt = time();
-            $expiredAt = time() + 2 * 60* 60; //Exprired after 2 hours
-            $role = "Admin";
-
-            // JWT token
-            $token = array (
-                "iat" => $issusedAt,
-                "eat" => $expiredAt,
-                "uae" =>$_SERVER['HTTP_USER_AGENT'],
-                "data" => array (
-                    "id" => $id_clean,
-                    "role" => $role
-                )
-            );
-
-            $jwt = JWT::encode($token, $_SERVER['HTTP_JWT_SERECT']);
             
-            $return['user'] = array(
-                "id" => $id_clean,
-                "loginAt" => $issusedAt,
-                "jwt" => $jwt,
-                "expiresAt" => $expiredAt
-            );
-            $return['status'] = "Login Successfully";
-            http_response_code(200);
+            $role = get_user_role($pdocon, $id_clean);
+
+            if(!empty($role)){
+                //JWT config
+                $issusedAt = time();
+                $expiredAt = time() + 2 * 60* 60; //Exprired after 2 hours
+
+                // JWT token
+                $token = array (
+                    "iat" => $issusedAt,
+                    "eat" => $expiredAt,
+                    "uae" =>$_SERVER['HTTP_USER_AGENT'],
+                    "data" => array (
+                        "id" => $id_clean,
+                        "role" => $role
+                    )
+                );
+
+                $jwt = JWT::encode($token, $_SERVER['HTTP_JWT_SERECT']);
+                
+                $return['user'] = array(
+                    "id" => $id_clean,
+                    "loginAt" => $issusedAt,
+                    "jwt" => $jwt,
+                    "expiresAt" => $expiredAt
+                );
+                $return['status'] = "Login Successfully";
+                http_response_code(200);
+            }
         }
         else {
             http_response_code(401);
