0b16de9086f9d7c1634e8f285ca5d6d9f70d0498
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-26 11:29:30 +0530

    feat: Added routes
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import { HOME } from './route';
+import { HOME, ADMIN_DASHBOARD, TEACHER_DASHBOARD, STUDENT_DASHBOARD } from './route';
 
 import Home from '../pages/Home';
+import { PrivateRoute } from 'jwt-auth-react';
+import Dashboard from '../pages/Dashboard';
 
 
 const RouteComponent = () => {
@@ -12,6 +14,9 @@ const RouteComponent = () => {
             <Router>
                 <Switch>
                     <Route path={HOME} exact component={Home} />
+                    <PrivateRoute path={ADMIN_DASHBOARD} component={Dashboard} login_path={HOME} />
+                    <PrivateRoute path={TEACHER_DASHBOARD} component={Dashboard} login_path={HOME} />
+                    <PrivateRoute path={STUDENT_DASHBOARD} component={Dashboard} login_path={HOME} />
                     {/* <Route path="*" component={}/> 404 */}
                 </Switch>
             </Router>
--- frontend/src/routes/route.js
+++ frontend/src/routes/route.js
@@ -1,1 +1,4 @@
 export const HOME = '/';
+export const STUDENT_DASHBOARD = '/student/dashloard'
+export const TEACHER_DASHBOARD = '/teacher/dashboard'
+export const ADMIN_DASHBOARD = '/admin/dashboard'
