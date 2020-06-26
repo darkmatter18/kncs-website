c7b3b6c8f4563199135e4c79857648e73367074f
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-06-26 11:28:12 +0530

    feat: Integrating AuthProvider
    Signed-off-by: Arkadip Bhattacharya <in2arkadipb13@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { AuthProvider } from 'jwt-auth-react'
 import { CssBaseline } from '@material-ui/core';
 import RouteComponent from './routes';
 
 const App = () => (
     <React.Fragment>
-        <CssBaseline />
-        <RouteComponent />
+        <AuthProvider>
+            <CssBaseline />
+            <RouteComponent />
+        </AuthProvider>
     </React.Fragment>
 );
 
