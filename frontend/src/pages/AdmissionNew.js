b2d7ffd48abe94ecb2d16c57518df8cba60adf9a
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-02 20:45:42 +0530

    feat: New and Existing Admission Component added

---
+++ frontend/src/pages/AdmissionNew.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const AdmissionNew = () => {
+    return (
+        <React.Fragment>
+            <Header/>
+
+        </React.Fragment>
+    )
+}
+
+export default AdmissionNew
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -15,6 +15,8 @@ import {
 import Home from '../pages/Home';
 import Dashboard from '../pages/Dashboard';
 import AdmissionHome from '../pages/AdmissionHome';
+import AdmissionNew from "../pages/AdmissionNew";
+import AdmissionExisting from "../pages/AdmissionExisting";
 
 
 const RouteComponent = () => {
@@ -23,8 +25,8 @@ const RouteComponent = () => {
             <Switch>
                 <Route path={HOME} component={Home} exact/>
                 <Route path={ADMISSION_HOME} component={AdmissionHome} exact/>
-                <Route path={ADMISSION_NEW} component={} exact/>
-                <Route path={ADMISSION_EXISTING} component={} exact/>
+                <Route path={ADMISSION_NEW} component={AdmissionNew} exact/>
+                <Route path={ADMISSION_EXISTING} component={AdmissionExisting} exact/>
                 <PrivateRoute component={Dashboard} path={ADMIN_DASHBOARD} loginPath={HOME} exact/>
                 <PrivateRoute component={Dashboard} path={TEACHER_DASHBOARD} loginPath={HOME} exact/>
                 <PrivateRoute component={Dashboard} path={STUDENT_DASHBOARD} loginPath={HOME} exact/>
