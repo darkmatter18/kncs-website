0c99e4cf86690ceb2e31e275a03ee0286b7c822f
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-02 21:29:54 +0530

    feat: AdmissionNew.js basic fields added

--- frontend/src/pages/AdmissionNew.js
+++ frontend/src/pages/AdmissionNew.js
@@ -5,6 +5,9 @@ import CardContent from "@material-ui/core/CardContent";
 import Container from "@material-ui/core/Container";
 import Paper from "@material-ui/core/Paper";
 import Typography from "@material-ui/core/Typography";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
 
 const AdmissionNew = () => {
     return (
@@ -12,11 +15,35 @@ const AdmissionNew = () => {
             <Header/>
             <AdmissionNewExistingSwitch routeId={0}/>
             <Container>
-                <Paper elevation={0}>
+                <Paper elevation={0} square>
                     <CardContent>
-                        <Typography variant={"body2"}>
-                            Fill the form bellow to Apply for Admission
+                        <Typography variant={"h6"}>
+                            Please Fill the form
                         </Typography>
+                        <Card variant={"outlined"}>
+                            <CardContent>
+                                <Grid container spacing={5} justify={"center"}>
+                                    <Grid item md={4}>
+                                        <TextField required fullWidth label={"First Name"} id={"first_name"} variant={"outlined"}/>
+                                    </Grid>
+                                    <Grid item md={4}>
+                                        <TextField required fullWidth label={"Middle Name"} id={"middle_name"} variant={"outlined"}/>
+                                    </Grid>
+                                    <Grid item md={4}>
+                                        <TextField required fullWidth label={"Last Name"} id={"last_name"} variant={"outlined"}/>
+                                    </Grid>
+                                </Grid>
+                                <Grid container style={{marginTop: 32}} spacing={5}>
+                                    <Grid item md={3}>
+                                        <TextField fullWidth required type={"email"} label={"E-Mail"} id={"email"} variant={"outlined"}/>
+                                    </Grid>
+                                    <Grid item md={3}>
+                                        <TextField fullWidth required label={"Mobile"} id={"mobile"} variant={"outlined"}/>
+                                    </Grid>
+
+                                </Grid>
+                            </CardContent>
+                        </Card>
                     </CardContent>
                 </Paper>
             </Container>
