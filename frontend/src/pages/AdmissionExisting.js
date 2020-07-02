36e90776d5e4594bf100e37828cdae1dde3a5825
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-02 21:11:57 +0530

    feat: New and Existing Routes are created

--- frontend/src/pages/AdmissionExisting.js
+++ frontend/src/pages/AdmissionExisting.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Header from "../components/Header";
+import AdmissionNewExistingSwitch from "../components/AdmissionNewExistingSwitch";
+
 
 const AdmissionExisting = () => {
     return (
         <React.Fragment>
             <Header/>
+            <AdmissionNewExistingSwitch routeId={1}/>
         </React.Fragment>
     )
 }
--- frontend/src/pages/AdmissionNew.js
+++ frontend/src/pages/AdmissionNew.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Header from "../components/Header";
+import AdmissionNewExistingSwitch from "../components/AdmissionNewExistingSwitch";
+import CardContent from "@material-ui/core/CardContent";
+import Container from "@material-ui/core/Container";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
 
 const AdmissionNew = () => {
     return (
         <React.Fragment>
             <Header/>
-
+            <AdmissionNewExistingSwitch routeId={0}/>
+            <Container>
+                <Paper elevation={0}>
+                    <CardContent>
+                        <Typography variant={"body2"}>
+                            Fill the form bellow to Apply for Admission
+                        </Typography>
+                    </CardContent>
+                </Paper>
+            </Container>
         </React.Fragment>
     )
 }
