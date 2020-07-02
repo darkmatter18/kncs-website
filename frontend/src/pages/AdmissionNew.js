92e40cdfd84b492b80b9582861e40aec14e336a1
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-02 22:15:33 +0530

    feat: Completed AdmissionNew.js UI

--- frontend/src/pages/AdmissionNew.js
+++ frontend/src/pages/AdmissionNew.js
@@ -8,8 +8,20 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import Button from "@material-ui/core/Button";
 
 const AdmissionNew = () => {
+
+    const [selectedDate, setSelectedDate] = React.useState(new Date());
+
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
     return (
         <React.Fragment>
             <Header/>
@@ -33,14 +45,46 @@ const AdmissionNew = () => {
                                         <TextField required fullWidth label={"Last Name"} id={"last_name"} variant={"outlined"}/>
                                     </Grid>
                                 </Grid>
-                                <Grid container style={{marginTop: 32}} spacing={5}>
+                                <Grid container style={{marginTop: 16}} spacing={5}>
+                                    <Grid item md={3}>
+                                        <TextField fullWidth required label={"Aadhar No"} id={"aadhar"} variant={"outlined"}/>
+                                    </Grid>
                                     <Grid item md={3}>
                                         <TextField fullWidth required type={"email"} label={"E-Mail"} id={"email"} variant={"outlined"}/>
                                     </Grid>
                                     <Grid item md={3}>
                                         <TextField fullWidth required label={"Mobile"} id={"mobile"} variant={"outlined"}/>
                                     </Grid>
+                                </Grid>
+                                <div style={{marginTop: 16}}>
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <KeyboardDatePicker
 
+                                            disableToolbar
+                                            variant="inline"
+                                            format="MM/dd/yyyy"
+                                            id="dob"
+                                            label="Date of Birth"
+                                            value={selectedDate}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                'aria-label': 'change date',
+                                            }}
+                                        />
+                                    </MuiPickersUtilsProvider>
+                                </div>
+
+                                <Grid container style={{marginTop: 16}} spacing={3}>
+                                    <Grid item>
+                                        <Button variant={"outlined"} color={"primary"}>
+                                            submit
+                                        </Button>
+                                    </Grid>
+                                    <Grid item>
+                                        <Button variant={"outlined"}  color={"secondary"}>
+                                            reset
+                                        </Button>
+                                    </Grid>
                                 </Grid>
                             </CardContent>
                         </Card>
