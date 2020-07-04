77f20ae469ecfae97843af07b361b4edfd2ac2df
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-04 19:45:16 +0530

    feat: AdmissionNewDone.js routes added

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -9,7 +9,7 @@ import {
     STUDENT_DASHBOARD,
     ADMISSION_HOME,
     ADMISSION_NEW,
-    ADMISSION_EXISTING
+    ADMISSION_EXISTING, ADMISSION_NEW_DONE
 } from './route';
 
 import Home from '../pages/Home';
@@ -17,6 +17,7 @@ import Dashboard from '../pages/Dashboard';
 import AdmissionHome from '../pages/AdmissionHome';
 import AdmissionNew from "../pages/AdmissionNew";
 import AdmissionExisting from "../pages/AdmissionExisting";
+import AdmissionNewDone from "../pages/AdmissionNewDone";
 
 
 const RouteComponent = () => {
@@ -26,6 +27,7 @@ const RouteComponent = () => {
                 <Route path={HOME} component={Home} exact/>
                 <Route path={ADMISSION_HOME} component={AdmissionHome} exact/>
                 <Route path={ADMISSION_NEW} component={AdmissionNew} exact/>
+                <Route path={ADMISSION_NEW_DONE} component={AdmissionNewDone} exact/>
                 <Route path={ADMISSION_EXISTING} component={AdmissionExisting} exact/>
                 <PrivateRoute component={Dashboard} path={ADMIN_DASHBOARD} loginPath={HOME} exact/>
                 <PrivateRoute component={Dashboard} path={TEACHER_DASHBOARD} loginPath={HOME} exact/>
