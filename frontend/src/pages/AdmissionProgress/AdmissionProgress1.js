4cca956b00d1abc909c79f9356d73fd1887c7f05
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-04 20:04:34 +0530

    feat: AdmissionProgress1.js boiilerplate added

---
+++ frontend/src/pages/AdmissionProgress/AdmissionProgress1.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../../components/Header";
+
+const AdmissionProgress1 = () => {
+    return (
+        <React.Fragment>
+            <Header/>
+        </React.Fragment>
+    )
+}
+
+export default AdmissionProgress1
