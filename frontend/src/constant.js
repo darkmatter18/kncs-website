b5a47de071ddce17044be605de9ffc56d3e0541e
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-04 19:06:23 +0530

    feat: More constant added

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,2 +1,7 @@
 /**Api Routes */
 export const API_ROUTE_LOGIN = '/login';
+export const PRE_REGISTRATION = '/preregistration'
+
+/**Other Constant */
+export const netState = {IDLE: 11110, BUSY: 11111, ERROR: 11112}
+export const RECAPTCHA_SITE_KEY = '6LdTUK0ZAAAAAG71t-pY6p2S57TMe0-Lb0wLKqoy'
