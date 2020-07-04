e6e87411f492dddd1397efe76a449eea0a13c442
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-04 21:16:56 +0530

    AdmissionProgress1.j added

--- frontend/src/pages/AdmissionProgress/AdmissionProgress1.js
+++ frontend/src/pages/AdmissionProgress/AdmissionProgress1.js
@@ -1,10 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import Header from "../../components/Header";
 
 const AdmissionProgress1 = () => {
+    let { user_id } = useParams();
     return (
         <React.Fragment>
             <Header/>
+            {user_id}
         </React.Fragment>
     )
 }
