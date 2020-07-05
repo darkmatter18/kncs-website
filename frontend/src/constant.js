a0b9071ec57ec1da1f62f6009ba769132f1571db
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-05 13:18:05 +0530

    add: 4th progress route

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -5,5 +5,5 @@ export const PRE_REGISTRATION_LOGIN = '/preregistration/login'
 
 /**Other Constant */
 export const netState = {IDLE: 11110, BUSY: 11111, ERROR: 11112}
-export const ADMISSION_PROGRESS = ['personal_info', 'academic_info', 'payment_info']
+export const ADMISSION_PROGRESS = ['personal_info', 'academic_info', 'payment_info', 'declaration']
 export const RECAPTCHA_SITE_KEY = '6LdTUK0ZAAAAAG71t-pY6p2S57TMe0-Lb0wLKqoy'
