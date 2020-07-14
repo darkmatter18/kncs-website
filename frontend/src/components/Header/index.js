aeeb0bfe8ae7a9189dc0bffdfb78305aa3f71ea0
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-14 12:33:00 +0530

    refactor: header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {Container} from "@material-ui/core";
 import {makeStyles} from "@material-ui/core/styles";
-import * as banner from './banner.png'
+import * as banner from '../../assets/banner.png'
 
 const useStyle = makeStyles((theme)=> ({
     header: {
