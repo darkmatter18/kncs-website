17fc081fec445b0d26522a834b106fe2b46126ac
manojitkarmakar <jony.karmakar.2013@gmail.com>
2020-07-14 13:28:43 +0530

    trying to resolve the error

--- backend/logics_xGRFqrAg3wMfGUM5js1x/student_preregistration.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/student_preregistration.php
@@ -21,23 +21,23 @@ if(isset($_INPUT['first_name']) && isset($_INPUT['middle_name']) && isset($_INPU
         $first_name_clean = Filter::String($_INPUT['first_name']);
         $middle_name_clean = Filter::String($_INPUT['middle_name']);
         $last_name_clean = Filter::String($_INPUT['last_name']);
-        $aadhar_no_clean = Filter::Int($_INPUT['aadhar_no']);
+        $aadhar_no_clean = Filter::String($_INPUT['aadhar_no']);
         $email_clean = Filter::Email($_INPUT['email']);
-        $mobile_clean = Filter::Int($_INPUT['mobile']);
+        $mobile_clean = Filter::String($_INPUT['mobile']);
         $dob_clean = Filter::String($_INPUT['dob']);
         
         $application_no = time();
         //PDO there is total 7 input
-
+        
         $smt = $pdocon->prepare('INSERT INTO student_preregistration_details(application_no, first_name, middle_name, last_name, aadhar_no, email, mobile, dob) VALUES(:application_no, :first_name, :middle_name, :last_name, :aadhar_no, :email, :mobile, :dob)');
         
         $smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
         $smt->bindParam(':first_name', $first_name_clean, PDO::PARAM_STR);
         $smt->bindParam(':middle_name', $middle_name_clean, PDO::PARAM_STR);
         $smt->bindParam(':last_name', $last_name_clean, PDO::PARAM_STR);
-        $smt->bindParam(':aadhar_no', $aadhar_no_clean, PDO::PARAM_INT);
+        $smt->bindParam(':aadhar_no', $aadhar_no_clean, PDO::PARAM_STR);
         $smt->bindParam(':email', $email_clean, PDO::PARAM_STR);
-        $smt->bindParam(':mobile', $mobile_clean, PDO::PARAM_INT);
+        $smt->bindParam(':mobile', $mobile_clean, PDO::PARAM_STR);
         $smt->bindParam(':dob', $dob_clean, PDO::PARAM_STR);
 
         if($smt->execute()){
