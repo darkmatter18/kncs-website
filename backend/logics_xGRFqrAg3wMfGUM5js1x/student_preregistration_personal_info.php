bcd07a0b1cc0def9c79fd53e8b775cc8e3780c2d
manojitkarmakar <jony.karmakar.2013@gmail.com>
2020-07-15 11:18:03 +0530

    personal info, academic info, family info complete

--- backend/logics_xGRFqrAg3wMfGUM5js1x/student_preregistration_personal_info.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/student_preregistration_personal_info.php
@@ -18,39 +18,47 @@ header('Content-Type: application/json');
 
 if( isset($_INPUT['gender']) && isset($_INPUT['religion']) && isset($_INPUT['caste']) && isset($_INPUT['mother_tongue'])
 && isset($_INPUT['apply_for_reserved_seat']) && isset($_INPUT['caste_certificate_no']) && isset($_INPUT['weather_bpl'])
-&& isset($_INPUT['bpl_card_no']) && isset($_INPUT['whatsapp_no']) && isset($_INPUT['father_name']) && isset($_INPUT['father_occupation'])
-&& isset($_INPUT['mother_name']) && isset($_INPUT['mother_occupation']) && isset($_INPUT['guardian_name']) && isset($_INPUT['guardian_occupation'])
-&& isset($_INPUT['guardian_same_father']) && isset($_INPUT['recaptcha_token']) ){
+&& isset($_INPUT['bpl_card_no']) && isset($_INPUT['whatsapp_no']) && isset($_INPUT['recaptcha_token']) ){
 
     if(checkRecaptcha($_INPUT['recaptcha_token'])){
+        //BASIC INFO--- POST
         $gender_clean = Filter::String($_INPUT['gender']);
         $religion_clean = Filter::String($_INPUT['religion']);
         $caste_clean = Filter::String($_INPUT['caste']);
+        $caste_certificate_no_clean = Filter::Int($_INPUT['caste_certificate_no']);
         $mother_tongue_clean = Filter::String($_INPUT['mother_tongue']);
         $apply_for_reserved_seat_clean = Filter::String($_INPUT['apply_for_reserved_seat']);
-        $caste_certificate_no_clean = Filter::String($_INPUT['caste_certificate_no']);
         $weather_bpl_clean = Filter::String($_INPUT['weather_bpl']);
-        $bpl_card_no_clean = Filter::String($_INPUT['bpl_card_no']);
-        $whatsapp_no_clean = Filter::String($_INPUT['whatsapp_no']);
-        $father_name_clean = Filter::String($_INPUT['father_name']);
-        $father_occupation_clean = Filter::String($_INPUT['father_occupation']);
-        $mother_name_clean = Filter::String($_INPUT['mother_name']);
-        $mother_occupation_clean = Filter::String($_INPUT['mother_occupation']);
-        $guardian_name_clean = Filter::String($_INPUT['guardian_name']);
-        $guardian_occupation_clean = Filter::String($_INPUT['guardian_occupation']);
-        $guardian_same_father_clean = Filter::String($_INPUT['guardian_same_father']);
-        $recaptcha_token_clean = Filter::String($_INPUT['recaptcha_token']);
+        $bpl_card_no_clean = Filter::Int($_INPUT['bpl_card_no']);
+        $whatsapp_no_clean = Filter::Int($_INPUT['whatsapp_no']);
+
+
 
         // $_clean = Filter::String($_INPUT['']);
 
-        $smt = $pdocon->prepare('INSERT INTO xxx () VALUES(:, )');
+        $smt = $pdocon->prepare('INSERT INTO student_preregistration_draft_basic_info(application_no, gender, religion, caste, mother_tongue,
+        apply_for_reserved_seat, caste_certificate_no,  weather_bpl, bpl_card_no, whatsapp_no) VALUES(:application_no, :gender, :religion, :caste, :mother_tongue,
+        :apply_for_reserved_seat, :caste_certificate_no,  :weather_bpl, :bpl_card_no, :whatsapp_no )');
         
-        $smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+        //$smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+        $smt->bindParam(':gender', $gender, PDO::PARAM_STR);
+        $smt->bindParam(':religion', $religion, PDO::PARAM_STR);
+        $smt->bindParam(':caste', $caste, PDO::PARAM_STR);
+        $smt->bindParam(':mother_tongue', $mother_tongue, PDO::PARAM_STR);
+        $smt->bindParam(':apply_for_reserved_seat', $apply_for_reserved_seat, PDO::PARAM_STR);
+        $smt->bindParam(':caste_certificate_no', $caste_certificate_no, PDO::PARAM_INT);
+        $smt->bindParam(':weather_bpl', $weather_bpl, PDO::PARAM_STR);
+        $smt->bindParam(':bpl_card_no', $bpl_card_no, PDO::PARAM_INT);
+        $smt->bindParam(':whatsapp_no', $whatsapp_no, PDO::PARAM_INT);
+        
+        //$smt->bindParam(':', $, PDO::PARAM_STR);
+
 
     }else{
+        http_response_code(401);
 
 
     }
 }else{
-    
+    http_response_code(400);    
 }
