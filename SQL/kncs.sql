0d695cd9c2cbe4edcd4c3e5b4e75096f5d3646c3
manojitkarmakar <jony.karmakar.2013@gmail.com>
2020-07-15 11:25:10 +0530

    changed SQL

--- SQL/kncs.sql
+++ SQL/kncs.sql
@@ -1,15 +1,16 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 14, 2020 at 05:03 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.5
+-- Generation Time: Jul 14, 2020 at 06:17 PM
+-- Server version: 10.1.35-MariaDB
+-- PHP Version: 7.2.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone = "+05:30";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -20,56 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `kncs`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `calender`
---
-
-CREATE TABLE `calender` (
-  `id` int(11) NOT NULL,
-  `day_of_week` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `class_detail`
---
-
-CREATE TABLE `class_detail` (
-  `id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Details to be filled';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `student_preregistration_academic_info`
---
-
-CREATE TABLE `student_preregistration_academic_info` (
-  `application_no` int(10) NOT NULL,
-  `previous_school_name` varchar(100) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `year_of_madhyamik` int(4) NOT NULL,
-  `previous_student_id` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `marks_beng` int(3) NOT NULL,
-  `marks_engb` int(3) NOT NULL,
-  `marks_maths` int(3) NOT NULL,
-  `marks_psc` int(3) NOT NULL,
-  `marks_lsc` int(3) NOT NULL,
-  `marks_geo` int(3) NOT NULL,
-  `marks_hist` int(3) NOT NULL,
-  `marks_total` int(4) NOT NULL,
-  `marks_percentage` int(6) NOT NULL,
-  `stream` varchar(20) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `first_language` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `second_language` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `first_major` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `second_major` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `third_major` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `forth_major` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
 
 -- --------------------------------------------------------
 
@@ -96,12 +47,12 @@ CREATE TABLE `student_preregistration_details` (
 
 CREATE TABLE `student_preregistration_draft_address` (
   `application_no` int(10) NOT NULL,
-  `address_line_1` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `address_line_2` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `city` varchar(30) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `district` varchar(25) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
+  `address_line_1` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `address_line_2` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `district` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pin` int(6) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -111,16 +62,16 @@ CREATE TABLE `student_preregistration_draft_address` (
 
 CREATE TABLE `student_preregistration_draft_basic_info` (
   `application_no` int(10) NOT NULL,
-  `gender` varchar(10) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `religion` varchar(15) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `caste` varchar(10) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `mother_tongue` varchar(15) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `apply_for_reserved_seat` varchar(10) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `caste_certificate_no` varchar(15) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `weather_bpl` varchar(10) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `bpl_card_no` varchar(15) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
+  `gender` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `religion` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `caste` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mother_tongue` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `apply_for_reserved_seat` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `caste_certificate_no` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `weather_bpl` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `bpl_card_no` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
   `whatsapp_no` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -130,14 +81,14 @@ CREATE TABLE `student_preregistration_draft_basic_info` (
 
 CREATE TABLE `student_preregistration_draft_family_info` (
   `application_no` int(10) NOT NULL,
-  `father_name` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `father_occupation` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `mother_name` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `mother_occupation` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `guardian_name` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `guardian_occupation` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL,
-  `guardian_same_father` varchar(50) COLLATE utf8mb4_unicode_nopad_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_nopad_ci;
+  `father_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `father_occupation` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mother_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `mother_occupation` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guardian_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guardian_occupation` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `guardian_same_father` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -154,50 +105,13 @@ CREATE TABLE `student_preregistration_login` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `subject_detail`
---
-
-CREATE TABLE `subject_detail` (
-  `id` int(11) NOT NULL,
-  `subject_name` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Details to be filled';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `teacher_detail`
---
-
-CREATE TABLE `teacher_detail` (
-  `id` int(11) NOT NULL,
-  `teacher name` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='Details to be filled';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `timetable`
---
-
-CREATE TABLE `timetable` (
-  `subject_id` int(11) NOT NULL,
-  `calender_date_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `teacher_id` int(11) NOT NULL,
-  `start_time` time NOT NULL,
-  `end_time` time NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `users_login`
 --
 
 CREATE TABLE `users_login` (
   `id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'User name of User',
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Password of the user',
-  `last-login` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'Last login Timestamp',
+  `last-login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Last login Timestamp',
   `last-login-ip` varchar(40) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Last login IP'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -231,24 +145,6 @@ INSERT INTO `users_role` (`id`, `role`) VALUES
 --
 
 --
--- Indexes for table `calender`
---
-ALTER TABLE `calender`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `class_detail`
---
-ALTER TABLE `class_detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `student_preregistration_academic_info`
---
-ALTER TABLE `student_preregistration_academic_info`
-  ADD PRIMARY KEY (`application_no`);
-
---
 -- Indexes for table `student_preregistration_details`
 --
 ALTER TABLE `student_preregistration_details`
@@ -279,27 +175,6 @@ ALTER TABLE `student_preregistration_login`
   ADD PRIMARY KEY (`application_no`);
 
 --
--- Indexes for table `subject_detail`
---
-ALTER TABLE `subject_detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `teacher_detail`
---
-ALTER TABLE `teacher_detail`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `timetable`
---
-ALTER TABLE `timetable`
-  ADD PRIMARY KEY (`subject_id`,`calender_date_id`,`class_id`,`teacher_id`,`start_time`),
-  ADD KEY `class_timetable_constraint` (`class_id`),
-  ADD KEY `teacher_timetable_constraint` (`teacher_id`),
-  ADD KEY `calender_timetable_constraint` (`calender_date_id`);
-
---
 -- Indexes for table `users_login`
 --
 ALTER TABLE `users_login`
@@ -319,16 +194,19 @@ ALTER TABLE `users_role`
 -- Constraints for table `student_preregistration_draft_address`
 --
 ALTER TABLE `student_preregistration_draft_address`
-  ADD CONSTRAINT `application_no_constraint` FOREIGN KEY (`application_no`) REFERENCES `student_preregistration_details` (`application_no`);
+  ADD CONSTRAINT `address_application_no_constraint` FOREIGN KEY (`application_no`) REFERENCES `student_preregistration_details` (`application_no`);
 
 --
--- Constraints for table `timetable`
+-- Constraints for table `student_preregistration_draft_basic_info`
 --
-ALTER TABLE `timetable`
-  ADD CONSTRAINT `calender_timetable_constraint` FOREIGN KEY (`calender_date_id`) REFERENCES `calender` (`id`),
-  ADD CONSTRAINT `class_timetable_constraint` FOREIGN KEY (`class_id`) REFERENCES `class_detail` (`id`),
-  ADD CONSTRAINT `subject_timetable_constraint` FOREIGN KEY (`subject_id`) REFERENCES `subject_detail` (`id`),
-  ADD CONSTRAINT `teacher_timetable_constraint` FOREIGN KEY (`teacher_id`) REFERENCES `teacher_detail` (`id`);
+ALTER TABLE `student_preregistration_draft_basic_info`
+  ADD CONSTRAINT `basic_info_application_no_constraint` FOREIGN KEY (`application_no`) REFERENCES `student_preregistration_details` (`application_no`);
+
+--
+-- Constraints for table `student_preregistration_login`
+--
+ALTER TABLE `student_preregistration_login`
+  ADD CONSTRAINT `login_application_no_constraint` FOREIGN KEY (`application_no`) REFERENCES `student_preregistration_details` (`application_no`);
 
 --
 -- Constraints for table `users_role`
