7dd0fe6ddff7805507275a825d079950a0f48ea6
manojitkarmakar <jony.karmakar.2013@gmail.com>
2020-07-16 22:14:43 +0530

    personal info modified

--- backend/logics_xGRFqrAg3wMfGUM5js1x/post_student_preregistration_personal_info.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/post_student_preregistration_personal_info.php
@@ -8,6 +8,7 @@
 
 define('_inc', true);
 require INC_DIR.'index.php';
+require INC_DIR.'protected.php';
 
 $_INPUT = json_decode(file_get_contents('php://input'), true);
 
@@ -19,30 +20,51 @@ header('Content-Type: application/json');
 
 if( isset($_INPUT['gender']) && isset($_INPUT['religion']) && isset($_INPUT['caste']) && isset($_INPUT['mother_tongue'])
 && isset($_INPUT['apply_for_reserved_seat']) && isset($_INPUT['caste_certificate_no']) && isset($_INPUT['weather_bpl'])
-&& isset($_INPUT['bpl_card_no']) && isset($_INPUT['whatsapp_no']) && isset($_INPUT['recaptcha_token']) ){
+&& isset($_INPUT['bpl_card_no']) && isset($_INPUT['whatsapp_no']) && isset($_INPUT['father_name']) && isset($_INPUT['father_occupation'])
+&& isset($_INPUT['mother_name']) && isset($_INPUT['mother_occupation']) && isset($_INPUT['guardian_name']) && isset($_INPUT['guardian_occupation'])
+&& isset($_INPUT['guardian_same_father']) && isset($_INPUT['address_line_1']) && isset($_INPUT['address_line_2']) && isset($_INPUT['city'])
+&& isset($_INPUT['district']) && isset($_INPUT['pin']) && isset($_INPUT['recaptcha_token']) ){
 
     if(checkRecaptcha($_INPUT['recaptcha_token'])){
-        //BASIC INFO--- POST
+        //TODO: Setup Application Id
+        $application_no=$auth_user["data"]["application_no"];
+
+        //BASIC INFO---9 INPUT
         $gender_clean = Filter::String($_INPUT['gender']);
         $religion_clean = Filter::String($_INPUT['religion']);
         $caste_clean = Filter::String($_INPUT['caste']);
-        $caste_certificate_no_clean = Filter::Int($_INPUT['caste_certificate_no']);
         $mother_tongue_clean = Filter::String($_INPUT['mother_tongue']);
         $apply_for_reserved_seat_clean = Filter::String($_INPUT['apply_for_reserved_seat']);
+        $caste_certificate_no_clean = Filter::Int($_INPUT['caste_certificate_no']);
         $weather_bpl_clean = Filter::String($_INPUT['weather_bpl']);
         $bpl_card_no_clean = Filter::Int($_INPUT['bpl_card_no']);
         $whatsapp_no_clean = Filter::Int($_INPUT['whatsapp_no']);
 
-        //TODO: Setup Application Id
-        $application_no=1234567890;
+        // FAMILY INFO ---7 INPUT
+        $father_name_clean = Filter::String($_INPUT['father_name']);
+        $father_occupation_clean = Filter::String($_INPUT['father_occupation']);
+        $mother_name_clean = Filter::String($_INPUT['mother_name']);
+        $mother_occupation_clean = Filter::String($_INPUT['mother_occupation']);
+        $guardian_name_clean = Filter::String($_INPUT['guardian_name']);
+        $guardian_occupation_clean = Filter::String($_INPUT['guardian_occupation']);
+        $guardian_same_father_clean = Filter::String($_INPUT['guardian_same_father']);
 
-        // $_clean = Filter::String($_INPUT['']);
+        //ADDRESS INFO 5 INPUT 
+        $address_line_1 = Filter::String($_INPUT['address_line_1']);
+        $address_line_2 = Filter::String($_INPUT['address_line_2']);
+        $city = Filter::String($_INPUT['city']);
+        $district = Filter::String($_INPUT['district']);
+        $pin = Filter::Int($_INPUT['pin']);
 
+
+        //INSERTING STUDENT BASIC INFO INTO DATABASE
         $smt = $pdocon->prepare('INSERT INTO student_preregistration_draft_basic_info(application_no, gender, religion, caste, mother_tongue,
-        apply_for_reserved_seat, caste_certificate_no,  weather_bpl, bpl_card_no, whatsapp_no) VALUES(:application_no, :gender, :religion, :caste, :mother_tongue,
-        :apply_for_reserved_seat, :caste_certificate_no,  :weather_bpl, :bpl_card_no, :whatsapp_no )');
+                                            apply_for_reserved_seat, caste_certificate_no,  weather_bpl, bpl_card_no, whatsapp_no)
+                                VALUES(:application_no, :gender, :religion, :caste, :mother_tongue, :apply_for_reserved_seat,
+                                        :caste_certificate_no,  :weather_bpl, :bpl_card_no, :whatsapp_no )');
         
-        //$smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+        $smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+        
         $smt->bindParam(':gender', $gender, PDO::PARAM_STR);
         $smt->bindParam(':religion', $religion, PDO::PARAM_STR);
         $smt->bindParam(':caste', $caste, PDO::PARAM_STR);
@@ -52,21 +74,60 @@ if( isset($_INPUT['gender']) && isset($_INPUT['religion']) && isset($_INPUT['cas
         $smt->bindParam(':weather_bpl', $weather_bpl, PDO::PARAM_STR);
         $smt->bindParam(':bpl_card_no', $bpl_card_no, PDO::PARAM_INT);
         $smt->bindParam(':whatsapp_no', $whatsapp_no, PDO::PARAM_INT);
-        
-        //$smt->bindParam(':', $, PDO::PARAM_STR);
 
         if($smt->execute()){
+            // INSERTING FAMILY DETAILS INTO DATABASE
+            $smt = $pdocon->prepare('INSERT INTO student_preregistration_draft_family_info
+            (application_no, father_name, father_occupation, mother_name, mother_occupation, guardian_name, guardian_occupation, guardian_same_father)
+            VALUES(:application_no, :father_name, :father_occupation, :mother_name, :mother_occupation, :guardian_name, :guardian_occupation, :guardian_same_father)');
 
+            $smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+            $smt->bindParam(':father_name', $father_name, PDO::PARAM_STR);
+            $smt->bindParam(':father_occupation', $father_occupation, PDO::PARAM_STR);
+            $smt->bindParam(':mother_name', $mother_name, PDO::PARAM_STR);
+            $smt->bindParam(':mother_occupation', $mother_occupation, PDO::PARAM_STR);
+            $smt->bindParam(':guardian_name', $guardian_name, PDO::PARAM_STR);
+            $smt->bindParam(':guardian_occupation', $guardian_occupation, PDO::PARAM_STR);
+            $smt->bindParam(':guardian_same_father', $guardian_same_father, PDO::PARAM_STR);
+            
+            if($smt->execute()){
+                // INSERTING ADDRESS INFO INTO DATABASE
+                $smt = $pdocon->prepare('INSERT INTO student_preregistration_draft_address(address_line_1, address_line_2, city, district, pin)
+                    VALUES(:address_line_1, :address_line_2, :city, :district, :pin)');
 
+            $smt->bindParam(':application_no', $application_no, PDO::PARAM_STR);
+            $smt->bindParam(':address_line_1', $address_line_1, PDO::PARAM_STR);
+            $smt->bindParam(':address_line_2', $address_line_2, PDO::PARAM_STR);
+            $smt->bindParam(':city', $city, PDO::PARAM_STR);
+            $smt->bindParam(':district', $district, PDO::PARAM_STR);
+            $smt->bindParam(':pin', $pin, PDO::PARAM_STR);
+
+                if($smt->execute()){
+                    $return['status'] = true;
+                    $return['statusText'] = "Successfully Inserted";
+                    $return['error'] = null;
+
+                }else{
+                    http_response_code(500);
+                    $return['status'] = false;
+                    $return['statusText'] = null;
+                    $return['application_no'] = null;
+                    $return['error'] = "Failed to record on Database";
+                }
+
+            }else{
+                http_response_code(500);
+            }
         }else{
             http_response_code(500);
 
         }
     }else{
         http_response_code(401);
-
-
     }
 }else{
     http_response_code(400);    
 }
+
+echo json_encode($return);
+exit;
