aa30fce2049723e4832377d0ff9c4ad7f9790656
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-16 12:13:49 +0530

    feat: Integrated new auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { AuthProvider } from 'jwt-auth-react'
+import { AuthProvider } from 'react-auth-jwt'
 import { CssBaseline } from '@material-ui/core';
 import RouteComponent from './routes';
 
 const App = () => (
     <React.Fragment>
-        <AuthProvider>
+        <AuthProvider cookieName={"admissionAuth"}>
             <CssBaseline />
             <RouteComponent />
         </AuthProvider>
