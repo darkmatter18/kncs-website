f8147fb711b8df8a8210aab4d7b5c140f4b2e508
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-16 12:18:31 +0530

    feat: PrivateRoute done

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import {PrivateRoute} from 'jwt-auth-react';
 
 import {
     HOME,
-    ADMIN_DASHBOARD,
-    TEACHER_DASHBOARD,
-    STUDENT_DASHBOARD,
     ADMISSION_HOME,
     ADMISSION_NEW,
     ADMISSION_EXISTING, ADMISSION_NEW_DONE, ADMISSION_PROGRESS_ROUTE
 } from './route';
 
 import Home from '../pages/Home';
-import Dashboard from '../pages/Dashboard';
 import AdmissionHome from '../pages/AdmissionHome';
 import AdmissionNew from "../pages/AdmissionNew";
 import AdmissionExisting from "../pages/AdmissionExisting";
 import AdmissionNewDone from "../pages/AdmissionNewDone";
 import AdmissionProgress from "../pages/AdmissionProgress";
+import {PrivateRoute} from "react-auth-jwt";
 
 
 const RouteComponent = () => {
@@ -30,11 +26,7 @@ const RouteComponent = () => {
                 <Route path={ADMISSION_NEW} component={AdmissionNew} exact/>
                 <Route path={ADMISSION_NEW_DONE} component={AdmissionNewDone} exact/>
                 <Route path={ADMISSION_EXISTING} component={AdmissionExisting} exact/>
-                <Route path={ADMISSION_PROGRESS_ROUTE} component={AdmissionProgress} exact/>
-                <PrivateRoute component={Dashboard} path={ADMIN_DASHBOARD} loginPath={HOME} exact/>
-                <PrivateRoute component={Dashboard} path={TEACHER_DASHBOARD} loginPath={HOME} exact/>
-                <PrivateRoute component={Dashboard} path={STUDENT_DASHBOARD} loginPath={HOME} exact/>
-                {/* <Route path="*" component={}/> 404 */}
+                <PrivateRoute path={ADMISSION_PROGRESS_ROUTE} Component={AdmissionProgress} loginPath={ADMISSION_NEW} exact/>
             </Switch>
         </Router>
     )
