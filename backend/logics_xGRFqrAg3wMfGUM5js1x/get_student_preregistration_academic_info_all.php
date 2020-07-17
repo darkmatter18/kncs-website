8c1d8f2c27e1385246564dc2505a7054968dc0ea
manojitkarmakar <jony.karmakar.2013@gmail.com>
2020-07-17 15:52:35 +0530

    get two pages & routes modified

--- backend/logics_xGRFqrAg3wMfGUM5js1x/get_student_preregistration_academic_info_all.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/get_student_preregistration_academic_info_all.php
@@ -3,12 +3,12 @@
 /**
  * GET
  * student_preregistration BASIC INFO processing logics
- * Manojit Karmakar (15/07/2020)
+ * Manojit Karmakar (17/07/2020)
  */
 
 define('_inc', true);
-require INC_DIR.'index.php';
-require INC_DIR.'protected.php';
+require INC_DIR . 'index.php';
+require INC_DIR . 'protected.php';
 
 $_INPUT = json_decode(file_get_contents('php://input'), true);
 
@@ -17,9 +17,10 @@ header('Content-Type: application/json');
 
 $application_no=$auth_user["data"]["application_no"];
 
-$smt = $pdocon->prepare("SELECT T2.previous_school_name, T2.year_of_madhyamik, T2.previous_student_id
-                        T3.marks_beng, T3.marks_engb, T3.marks_maths, T3.marks_psc, T3.marks_lsc, T3.marks_geo, T3.marks_hist, T3.marks_total, T3.marks_percentage
-                        T4.stream. T4.first_language, T4.second_language, T4.first_major, T4.second_major, T4.third_major, T4.forth_major
+$smt = $pdocon->prepare("SELECT T2.previous_school_name, T2.year_of_madhyamik, T2.previous_student_id,
+                                T3.marks_beng, T3.marks_engb, T3.marks_maths, T3.marks_psc, T3.marks_lsc, T3.marks_geo, T3.marks_hist,
+                                T3.marks_total, T3.marks_percentage,
+                                T4.stream, T4.first_language, T4.second_language, T4.first_major, T4.second_major, T4.third_major, T4.forth_major
                         
                         FROM 'student_preregistration_details' AS T1
                         
@@ -32,8 +33,17 @@ $smt = $pdocon->prepare("SELECT T2.previous_school_name, T2.year_of_madhyamik, T
                         INNER JOIN 'student_preregistration_draft_present_academic' AS T4
                         ON T1.application_no=T4.application_no
 
-                        WHERE T1.application_no=:application_no");
+                        WHERE T1.application_no = :application_no");
                     
-$smt->bindParam(':application_no', $application_no, PDO::PARAM_INT);
+// $smt->bindParam(':application_no', $application_no, PDO::PARAM_INT);
 
+if($smt->execute([':application_no'])){
+    $smt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $output = $smt->fetch();
+    $output['first_name'];
+
+}else{
+
+}
 
--- backend/logics_xGRFqrAg3wMfGUM5js1x/get_student_preregistration_personal_info.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/get_student_preregistration_personal_info.php
@@ -2,40 +2,53 @@
 
 /**
  * GET
- * student_preregistration BASIC INFO processing logics
- * Manojit Karmakar (15/07/2020)
+ * GETTING STUDENT PREREGISTRATION  PERSONAL DETAILS FROM DATABASE
+ * DATABASE : kncs
+ *    TABLE : student_preregistration_details AS T1
+ *            student_preregistration_draft_basic_info AS T2
+ *            student_preregistration_draft_family_info AS T3
+ *            student_preregistration_draft_address AS T4
+ * Manojit Karmakar (17/07/2020)
  */
 
 define('_inc', true);
-require INC_DIR.'index.php';
+require INC_DIR . 'index.php';
+require INC_DIR . 'protected.php';
 
 $_INPUT = json_decode(file_get_contents('php://input'), true);
 
 $return = [];
 header('Content-Type: application/json');
 
-//$smt = $pdocon->prepare
-
-$application_no=1234567890;
+$application_no = $auth_user["data"]["application_no"];
 
 $smt = $pdocon->prepare("SELECT T1.first_name, T1.middle_name, T1.last_name, T1.aadhar_no, T1.email, T1.mobile, T1.dob,
-                        T2.gender, T2.religion. T2.caste, T2.mother_tongue, T2.apply_for_reserved_seat, T2.caste_certificate_no, T2.weather_bpl, T2.bpl_card_no, 
-                        T3.father_name, T3.father_name, T3.father_occupation, T3.mother_name, T3.mother_occupation, T3.guardian_name, T3.guardian_occupation, T3.guardian_same_father
-                        T4.address_line_1, T4.address_line_2, T4.city, T4.district, T4.pin
-                        
+                                T2.gender, T2.religion, T2.caste, T2.mother_tongue, T2.apply_for_reserved_seat, T2.caste_certificate_no,
+                                T2.weather_bpl, T2.bpl_card_no, T2.whatsapp_no,
+                                T3.father_name, T3.father_occupation, T3.mother_name, T3.mother_occupation, T3.guardian_name,
+                                T3.guardian_occupation, T3.guardian_same_father,
+                                T4.address_line_1, T4.address_line_2, T4.city, T4.district, T4.pin
                         FROM 'student_preregistration_details' AS T1
-                        
-                        INNER JOIN `student_preregistration_draft_basic_info` AS T2
+                        LEFT OUTER JOIN `student_preregistration_draft_basic_info` AS T2
                         ON T1.application_no=T2.application_no
-
-                        INNER JOIN `student_preregistration_draft_family_info` AS T3
+                        LEFT OUTER JOIN `student_preregistration_draft_family_info` AS T3
                         ON T1.application_no=T3.application_no
-
-                        INNER JOIN 'student_preregistration_draft_address' AS T4
+                        LEFT OUTER JOIN 'student_preregistration_draft_address' AS T4
                         ON T1.application_no=T4.application_no
-
-                        WHERE T1.application_no=:application_no");
+                        WHERE T1.application_no = :application_no");
                     
-$smt->bindParam(':application_no', $application_no, PDO::PARAM_INT);
+//$smt->bindParam(':application_no', $application_no, PDO::PARAM_INT);
+
+if($smt->execute([':application_no'])){
+    $smt->setFetchMode(PDO::FETCH_ASSOC);
+
+    $output = $smt->fetch();
+    $output['first_name'];
+
+}else{
+
+}
+
+
 
 
