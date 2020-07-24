18b75ca00c251c6503fd7957c7e7bc93c41b4d76
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-24 20:07:25 +0530

    feat: Output Added

--- backend/logics_xGRFqrAg3wMfGUM5js1x/post/user_login_process.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/post/user_login_process.php
@@ -20,44 +20,53 @@ if (isset($_INPUT['email']) && isset($_INPUT['password']) && isset($_INPUT['reca
         $pdocon->beginTransaction();
 
         $email_clean = Filter::Email($_INPUT['email']);
-        $password_clean = Filter::String($_INPUT['password']);
 
         $time = time();
         $ip = get_client_ip();
 
-        $smt = $pdocon->prepare("SELECT id, password FROM `users_login` WHERE id = :email AND password = :password LIMIT 1");
+        $smt = $pdocon->prepare("SELECT id, password FROM `users_login` WHERE id = :email LIMIT 1");
 
         $smt->bindParam(":email", $email_clean, PDO::PARAM_STR);
-        $smt->bindParam(":password", $password_clean, PDO::PARAM_STR);
 
         if ($smt->execute()){
+            $_d  = $smt->fetch(PDO::FETCH_ASSOC);
+            if($_d){
 
-            $smt = $pdocon->prepare("UPDATE users_login SET last_login = :time , last_login_ip= :ip WHERE id = :email ");
 
-            $smt->bindParam(':time', $time, PDO::PARAM_STR);
-            $smt->bindParam(':ip', $ip, PDO::PARAM_STR);
-            $smt->bindParam(":email", $email_clean, PDO::PARAM_STR);
+                $smt = $pdocon->prepare("UPDATE users_login SET last_login = :time , last_login_ip= :ip WHERE id = :email ");
 
-            if ($smt->execute()) {
-                if($pdocon->commit()){
+                $smt->bindParam(':time', $time, PDO::PARAM_STR);
+                $smt->bindParam(':ip', $ip, PDO::PARAM_STR);
+                $smt->bindParam(":email", $email_clean, PDO::PARAM_STR);
 
-                    $return['status'] = true;
-                    $return['statusText'] = null;
-                    $return['error'] = "Login Successfull and table Updated";
+                if ($smt->execute()) {
+                    if($pdocon->commit()){
+
+                        $return['status'] = true;
+                        $return['statusText'] = null;
+                        $return['error'] = "Login Successfull and table Updated";
+                    } else {
+
+                        $return['status'] = false;
+                        $return['statusText'] = null;
+                        $return['error'] = "Failed to commit";
+                    }
+
+
                 } else {
-
+                    http_response_code(500);
                     $return['status'] = false;
                     $return['statusText'] = null;
-                    $return['error'] = "Failed to commit";
+                    $return['error'] = "Failed to record on database";
+
                 }
-                
-
             } else {
-                http_response_code(500);
+                http_response_code(401);
                 $return['status'] = false;
+                $return['jwt'] = null;
+                $return['application_no'] = null;
                 $return['statusText'] = null;
-                $return['error'] = "Failed to record on database";
-
+                $return['error'] = "Invalid E-mail Id";
             }
         } else {
             http_response_code(500);
