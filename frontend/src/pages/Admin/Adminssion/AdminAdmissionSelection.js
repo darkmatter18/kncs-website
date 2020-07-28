642c29ca7e23531394edcff81f4f6561b8d91b9e
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-28 12:59:54 +0530

    feat: AdminAdmissionSelection.js added

---
+++ frontend/src/pages/Admin/Adminssion/AdminAdmissionSelection.js
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardHeader from "../../Dashboard/DashboardHeader";
+
+const AdminAdmissionSelection = () => {
+    return (
+        <React.Fragment>
+            <DashboardHeader/>
+        </React.Fragment>
+    )
+}
+
+export default AdminAdmissionSelection
