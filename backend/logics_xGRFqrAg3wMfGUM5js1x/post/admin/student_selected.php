66df3689a23c9e0bbd5a645ab03ff79aa617b31c
pranjalb21 <53044379+pranjalb21@users.noreply.github.com>
2020-07-30 19:38:37 +0530

    Post and fetch done

--- backend/logics_xGRFqrAg3wMfGUM5js1x/post/admin/student_selected.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/post/admin/student_selected.php
@@ -8,18 +8,59 @@
 
     $return = [];
     header('Content-Type: application/json');
+    function execution()
+
+    if (isset($_INPUT['application_no'])
+    {
+        $pdocon->beginTransaction();
         
         $smt = $pdocon->prepare("UPDATE `student_preregistration_details` SET status = 'SELECTED' WHERE application_no = :application_no");
+        $smt->bind_param(':application_no', $application, PDO::PARAM_INT)
+        
 
-        if ($smt->execute()) 
+        foreach ($_INPUT['application_no'] as $number)
         {
-            $output = $smt->fetchAll(PDO::FETCH_ASSOC);
-            $return['data'] = $output;
-            $return['status'] = true;
-            $return['statusText'] = "Update Done (SELECTED)";
-            $return['error'] = null;
+            $application = $number;
+            $smt->ececute();
+        }
+        $x = $smt->commit()
 
-        } 
+        if ($x)
+        {
+            $smt = $pdocon->prepare("SELECT T1.application_no, T1.first_name, T1.middle_name, T1.last_name, T1.status,
+                                T2.previous_school_name, T2.year_of_madhyamik, T2.previous_student_id,
+                                T3.marks_beng, T3.marks_engb, T3.marks_maths, T3.marks_psc, T3.marks_lsc, T3.marks_geo,
+                                T3.marks_hist, T3.marks_total, T3.marks_percentage,
+                                T4.stream, T4.first_language, T4.second_language, T4.first_major,
+                                T4.second_major, T4.third_major, T4.forth_major, T4.direct_admission, T4.medium,
+                                T5.father_name
+                                FROM `student_preregistration_details` AS T1
+                                INNER JOIN `student_preregistration_draft_previous_academic_info` AS T2
+                                    ON T1.application_no=T2.application_no
+                                INNER JOIN `student_preregistration_draft_previous_academic_marks` AS T3
+                                    ON T1.application_no=T3.application_no
+                                INNER JOIN `student_preregistration_draft_present_academic` AS T4
+                                    ON T1.application_no=T4.application_no
+                                INNER JOIN student_preregistration_draft_family_info AS T5 
+                                    ON T1.application_no = T5.application_no
+                                WHERE T1.status='SELECTED' OR T1.Status='SUBMITTED' ");
+            if ($smt->execute()) 
+            {
+                $output = $smt->fetchAll(PDO::FETCH_ASSOC);
+                $return['data'] = $output;
+                $return['status'] = true;
+                $return['statusText'] = "Fetch Done (SUBMITTED , SELECTED)";
+                $return['error'] = null;
+        
+            }
+            else 
+            {
+                http_response_code(500);
+                $return['status'] = false;
+                $return['statusText'] = null;
+                $return['error'] = "Unable to connect database";
+            }
+        }
         else 
         {
             http_response_code(500);
@@ -27,6 +68,6 @@
             $return['statusText'] = null;
             $return['error'] = "Unable to connect database";
         }
-
+    }
     echo json_encode($return);
     exit;
