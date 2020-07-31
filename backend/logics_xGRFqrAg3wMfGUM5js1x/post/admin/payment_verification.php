4b94369c9c069ef2c5e1fbd6d1e6431bb17a3b11
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-07-31 13:09:39 +0530

    fix: payment_verification.php

--- backend/logics_xGRFqrAg3wMfGUM5js1x/post/admin/payment_verification.php
+++ backend/logics_xGRFqrAg3wMfGUM5js1x/post/admin/payment_verification.php
@@ -1,8 +1,10 @@
 <?php
 
-/*payment verification process ...
-*created my Manojit Karmakar 31.07.2020
-*/
+/**
+ * Payment Verification
+ * Created by - Manojit Karmakar 31.07.2020
+ * Modified By - Arkadip Bhattacharya on 21/07/2020
+ */
 
 define('_inc', true);
 require INC_DIR . 'index.php';
@@ -13,59 +15,70 @@ $_INPUT = json_decode(file_get_contents('php://input'), true);
 $return = [];
 header('Content-Type: application/json');
 
-if (isset($_INPUT['application_no']) ) {
-    $pdocon->beginTransaction();
-    
+if (isset($_INPUT['application_no'])) {
+    if (!empty($pdocon)) {
+        $pdocon->beginTransaction();
 
-    foreach ($_input['application_no'] as $application_no) {
-        $application_no_clean = Filter::Int($application_no);
-        $smt = $pdocon->prepare("UPDATE student_preregistration_draft_payment_info SET verified_transaction = 'Y' 
+
+        foreach ($_INPUT['application_no'] as $application_no) {
+            $application_no_clean = Filter::Int($application_no);
+            $smt = $pdocon->prepare("UPDATE student_preregistration_draft_payment_info SET verified_transaction = 'Y' 
                                                                                 WHERE application_no = :application_no ");
-        $smt->bindParam(":application_no", $application_no_clean, PDO::PARAM_INT);
-        $smt-> execute();
-    }
-     
-    if ($pdocon->commit()) {
+            $smt->bindParam(":application_no", $application_no_clean, PDO::PARAM_INT);
+            $smt->execute();
+        }
 
-        $smt = $pdocon->prepare("SELECT T1.first_name, T1.middle_name, T1.last_name, T2.previous_school_name, T2.year_of_madhyamik, T2.previous_student_id,
-        T3.marks_beng, T3.marks_engb, T3.marks_maths, T3.marks_psc, T3.marks_lsc, T3.marks_geo,
-        T3.marks_hist, T3.marks_total, T3.marks_percentage,
-        T4.stream, T4.first_language, T4.second_language, T4.first_major,
-        T4.second_major, T4.third_major, T4.forth_major, T4.direct_admission, T4.medium
-        FROM `student_preregistration_details` AS T1
-        INNER JOIN `student_preregistration_draft_previous_academic_info` AS T2
-            ON T1.application_no=T2.application_no
-        INNER JOIN `student_preregistration_draft_previous_academic_marks` AS T3
-            ON T1.application_no=T3.application_no
-        INNER JOIN `student_preregistration_draft_present_academic` AS T4
-            ON T1.application_no=T4.application_no
+        if ($pdocon->commit()) {
 
-        WHERE T1.status='SELECTED' OR T1.Status='SUBMITTED' ");
+            $smt = $pdocon->prepare("SELECT T1.application_no, T1.first_name, T1.middle_name, T1.last_name, T1.status,
+                                    T2.previous_school_name, T2.year_of_madhyamik, T2.previous_student_id,
+                                    T3.marks_beng, T3.marks_engb, T3.marks_maths, T3.marks_psc, T3.marks_lsc, T3.marks_geo,
+                                    T3.marks_hist, T3.marks_total, T3.marks_percentage,
+                                    T4.stream, T4.first_language, T4.second_language, T4.first_major,
+                                    T4.second_major, T4.third_major, T4.forth_major, T4.direct_admission, T4.medium,
+                                    T5.father_name, T6.mode_of_payment, T6.name_of_bank, T6.transaction_id, 
+                                    T6.transaction_date, T6.verified_transaction
+                                    FROM `student_preregistration_details` AS T1
+                                    INNER JOIN `student_preregistration_draft_previous_academic_info` AS T2
+                                        ON T1.application_no=T2.application_no
+                                    INNER JOIN `student_preregistration_draft_previous_academic_marks` AS T3
+                                        ON T1.application_no=T3.application_no
+                                    INNER JOIN `student_preregistration_draft_present_academic` AS T4
+                                        ON T1.application_no=T4.application_no
+                                    INNER JOIN student_preregistration_draft_family_info AS T5 
+                                        ON T1.application_no = T5.application_no 
+                                    INNER JOIN student_preregistration_draft_payment_info AS T6 
+                                        ON T1.application_no = T6.application_no
+                                    WHERE T1.status='SELECTED' OR T1.Status='SUBMITTED' ");
 
             if ($smt->execute()) {
 
-            $output = $smt->fetchAll(PDO::FETCH_ASSOC);
-            $return['data'] = $output;
-            $return['status'] = true;
-            $return['statusText'] = "Fetch Done (SUBMITTED , SELECTED)";
-            $return['error'] = null;
+                $output = $smt->fetchAll(PDO::FETCH_ASSOC);
+                $return['data'] = $output;
+                $return['status'] = true;
+                $return['statusText'] = "Fetch Done (SUBMITTED , SELECTED)";
+                $return['error'] = null;
 
             } else {
+                http_response_code(500);
+                $return['status'] = false;
+                $return['statusText'] = null;
+                $return['error'] = "Unable to connect database";
+            }
+
+        } else {
             http_response_code(500);
             $return['status'] = false;
             $return['statusText'] = null;
-            $return['error'] = "Unable to connect database";
-            }
+            $return['error'] = "Failed to commit record on Database";
 
+        }
     } else {
+        http_response_code(500);
         $return['status'] = false;
         $return['statusText'] = null;
         $return['error'] = "Failed to commit record on Database";
-
     }
-
-
-
 } else {
     http_response_code(400);
     $return['status'] = false;
