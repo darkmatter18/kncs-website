5b6c67ca3f2bf855b4517d77b576f41bd07cb603
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-18 11:50:17 +0530

    refactor: HeaderWithDrawer.js generalised

--- frontend/src/components/Headers/HeaderWithDrawer.js
+++ frontend/src/components/Headers/HeaderWithDrawer.js
@@ -1,30 +1,10 @@
 import React from "react"
 import Header from "./BasicHeader";
-import {useAuth, useSignOut} from "react-auth-jwt";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import SwipeableDrawer from "@material-ui/core/SwipeableDrawer";
 
-const HeaderWithDrawer = () => {
-    const signOut = useSignOut()
-    const auth = useAuth()
+const HeaderWithDrawer = ({rightLinks, drawerElements}) => {
     const [menuAnchor, setMenuAnchor] = React.useState(false)
 
-    //const {first_name, last_name} = auth().authState
-    const first_name = "A"
-    const last_name = "B"
-    const nameInitial = first_name.charAt(0).toUpperCase() + last_name.charAt(0).toUpperCase()
-    const headerLinks = [
-        (
-            <Avatar>{nameInitial}</Avatar>
-        ),
-        (
-            <IconButton onClick={() => signOut()} aria-label="show 17 new notifications" color="inherit">
-                <ExitToAppIcon/>
-            </IconButton>
-        )
-    ]
 
     const toggleDrawer = (open) => (event) => {
         if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -35,14 +15,14 @@ const HeaderWithDrawer = () => {
 
     return (
         <React.Fragment>
-            <Header leftMenuClickListener={toggleDrawer(true)} rightLinks={headerLinks}/>
+            <Header leftMenuClickListener={toggleDrawer(true)} rightLinks={rightLinks}/>
             <SwipeableDrawer
                 anchor={"left"}
                 open={menuAnchor}
                 onClose={toggleDrawer(false)}
                 onOpen={toggleDrawer(true)}
             >
-                Hello
+                {drawerElements}
             </SwipeableDrawer>
         </React.Fragment>
     )
