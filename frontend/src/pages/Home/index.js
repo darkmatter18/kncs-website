3c936109ddccfbc84d0028e42e7d60d7ce269272
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-23 11:50:26 +0530

    feat: Home to Login Redirection fix: Material table failing

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,6 +3,8 @@ import {makeStyles} from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Grid from "@material-ui/core/Grid";
 import Footer from "../../components/Footer";
+import {LOGIN} from "../../routes/route";
+import {useHistory} from 'react-router-dom'
 
 const useStyles = makeStyles(() => ({
     root: {
@@ -16,22 +18,30 @@ const useStyles = makeStyles(() => ({
 
 const Home = () => {
     const classes = useStyles()
-    return (
-        <React.Fragment>
-            <Grid container alignItems={"center"} justify={"center"} alignContent={"center"} className={classes.root}>
-                <Grid item md={12}>
-                    <Typography variant={"h4"} align={"center"} color={"textPrimary"}>
-                        Student Management Portal
-                    </Typography>
-                    <br/>
-                    <Typography variant={"h6"} align={"center"} color={"textSecondary"}>
-                        Go To notice and then click admission notice link
-                    </Typography>
+    const history = useHistory()
+    const re = true
+    if(re) {
+        history.push(LOGIN)
+        return <React.Fragment/>
+    }
+    else {
+        return (
+            <React.Fragment>
+                <Grid container alignItems={"center"} justify={"center"} alignContent={"center"} className={classes.root}>
+                    <Grid item md={12}>
+                        <Typography variant={"h4"} align={"center"} color={"textPrimary"}>
+                            Student Management Portal
+                        </Typography>
+                        <br/>
+                        <Typography variant={"h6"} align={"center"} color={"textSecondary"}>
+                            Go To notice and then click admission notice link
+                        </Typography>
+                    </Grid>
                 </Grid>
-            </Grid>
-            <Footer Ftype={'absolute'}/>
-        </React.Fragment>
-    )
+                <Footer Ftype={'absolute'}/>
+            </React.Fragment>
+        )
+    }
 }
 
 export default Home;
