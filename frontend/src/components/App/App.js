9484bb25f08a8be8daaf23f16179ac66eb3bd21a
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-08-31 16:51:35 +0530

    feat: App.js App.test.js moved

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import { AuthProvider } from 'react-auth-jwt'
 import { CssBaseline } from '@material-ui/core';
-import RouteComponent from './routes';
+import RouteComponent from '../../routes';
 import {ThemeProvider} from "@material-ui/styles";
-import lightBlue from "@material-ui/core/colors/lightBlue";
-import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
-
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            main: lightBlue[400],
-        }
-    },
-});
+import theme from "./theme";
 
 const App = () => (
     <React.Fragment>
