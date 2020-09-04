6426f3c8474bfddee8b0bb9548107b689c992753
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-09-04 11:13:06 +0530

    feat: index.js for header is ready

---
+++ frontend/src/lib/HeaderComponents/index.js
@@ -0,0 +1,5 @@
+import Header from './BasicHeader'
+import HeaderWithDrawer from "./HeaderWithDrawer";
+import MiniInternalDrawer from "./MiniInternalDrawer";
+
+export {Header, MiniInternalDrawer, HeaderWithDrawer};
