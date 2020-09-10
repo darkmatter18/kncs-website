04cbf7959d45cfbdcd2cbfdd330ab0299abbed6b
Arkadip Bhattacharya <in2arkadipb13@gmail.com>
2020-09-10 22:01:19 +0530

    feat: handling 400 errors

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,2 +1,7 @@
 <?php
-(require __DIR__ . '/../config/bootstrap.php')->run();
+try {
+    (require __DIR__ . '/../config/bootstrap.php')->run();
+} catch (Throwable $exception){
+    http_response_code(400);
+    echo sprintf('Bad Request: %s', $exception->getMessage());
+}
